b10a24d813e3623e27aacbe8bd9a2cf6ae3783a4
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2023-12-22 15:48:58 -0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -24,7 +24,7 @@ import os
 import tempfile
 
 # Define variable to hold llama2 weights namingfiner
-name = "meta-llama/Llama-2-7b"
+name = "meta-llama/Llama-2-7b-hf"
 # Set auth token variable from hugging face
 auth_token = "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"
 
