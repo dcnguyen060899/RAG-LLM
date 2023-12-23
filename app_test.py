bca55a3c65f80c33c64ec8e53b12a2b7683361c5
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2023-12-22 16:14:56 -0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -24,7 +24,7 @@ import os
 import tempfile
 
 # Define variable to hold llama2 weights namingfiner
-name = "gpt2"
+name = "gpt2-medium"
 # Set auth token variable from hugging face
 auth_token = "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"
 
