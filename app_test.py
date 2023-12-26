aaf1d46e1374874af2a3e3a34d08851ce798367a
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2023-12-25 18:42:56 -0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -54,35 +54,34 @@ For a more advanced model demonstration using Llama 2 7B, check out our Colab no
 
 
 
-# Initialize the SimpleInputPrompt with an empty template
-query_wrapper_prompt = SimpleInputPrompt("{query_str}")
+# # Initialize the SimpleInputPrompt with an empty template
+# query_wrapper_prompt = SimpleInputPrompt("{query_str}")
 
 # Streamlit UI to let the user update the system prompt
-# Start with an empty string or a default prompt
-default_prompt = ""
-user_system_prompt = st.text_area("How can I best assist you?", value="", height=100)
-update_button = st.button('Request')
+# # Start with an empty string or a default prompt
+# default_prompt = ""
+# user_system_prompt = st.text_area("How can I best assist you?", value="", height=100)
+# update_button = st.button('Request')
 
 # Initialize the llm object with a placeholder or default system prompt
 llm = HuggingFaceLLM(
-    context_window=4096,
+    context_window=1024,
     max_new_tokens=256,
-    system_prompt="",   # Placeholder if your initial prompt is empty
     query_wrapper_prompt=query_wrapper_prompt,  # Placeholder string
     model=model,
     tokenizer=tokenizer
 )
 
-# Function to update the system prompt and reinitialize the LLM with the new prompt
-def update_system_prompt(new_prompt):
-    global llm
-    llm.system_prompt = new_prompt
+# # Function to update the system prompt and reinitialize the LLM with the new prompt
+# def update_system_prompt(new_prompt):
+#     global llm
+#     llm.system_prompt = new_prompt
 
 
-if update_button:
-    # Update the system prompt and reinitialize the LLM
-    update_system_prompt(user_system_prompt)
-    st.success('Requested')
+# if update_button:
+#     # Update the system prompt and reinitialize the LLM
+#     update_system_prompt(user_system_prompt)
+#     st.success('Requested')
     
 # check if user request save to the memory
 # st.write(llm.system_prompt)
