c356b295972fbb81b884f6d5aa2636ca677fae06
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2023-12-25 21:06:09 -0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -112,7 +112,7 @@ embeddings=LangchainEmbedding(
 
 # Create new service context instance
 service_context = ServiceContext.from_defaults(
-    chunk_size=512,
+    chunk_size=1024,
     llm=llm,
     embed_model=embeddings
 )
